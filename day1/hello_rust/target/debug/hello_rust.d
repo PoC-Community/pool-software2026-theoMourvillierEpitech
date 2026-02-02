18db3805d034e35c7023668e0e43d0916d035a94
theo <theo@desktop-di2n04s.home>
2026-02-02 16:19:26 +0100

    hello_rust

---
+++ day1/hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+/home/theo/Bureau/poolSoftware/day1/hello_rust/target/debug/hello_rust: /home/theo/Bureau/poolSoftware/day1/hello_rust/src/hello_world.rs /home/theo/Bureau/poolSoftware/day1/hello_rust/src/main.rs
